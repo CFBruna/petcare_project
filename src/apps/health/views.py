c5ed45aa0fd6cc86007f7e7fc6ca7b1986f81af7
Bruna Menezes <brunaads.ti@gmail.com>
2025-08-17 02:02:12 -0300

    feat(health): implement logic for HealthRecordViewSet
    - Overrides get_queryset to ensure users can only view health
    records belonging to their own pets.
    
    - Implements perform_create to automatically associate the new
    health record with the currently authenticated user.

--- src/apps/health/views.py
+++ src/apps/health/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import HealthRecord
 from .serializers import HealthRecordSerializer
 
 
 class HealthRecordViewSet(viewsets.ModelViewSet):
-    queryset = HealthRecord.objects.all()
     serializer_class = HealthRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        return HealthRecord.objects.filter(pet__owner__user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
