f927952bc290351b11f0c1c67e755a456026872f
Bruna <brunaads.ti@gmail.com>
2025-08-18 23:55:24 +0000

    feat(health): customize admin for HealthRecord model

--- src/apps/health/admin.py
+++ src/apps/health/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import HealthRecord
 
-admin.site.register(HealthRecord)
+
+@admin.register(HealthRecord)
+class HealthRecordAdmin(admin.ModelAdmin):
+    list_display = ["pet", "record_type", "record_date", "created_by"]
+    search_fields = ["pet"]
